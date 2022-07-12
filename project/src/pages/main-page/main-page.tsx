890d431cc663b8be3109fc8225ccec109e46a878
dimpulsha <dimpulsha@gmail.com>
2022-07-12 18:32:24 +0300

    добавляет компонент Logo и подключает

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,4 +1,5 @@
 import PlaceCard from '../../components/place-card/place-card';
+import Logo from '../../components/logo/logo';
 
 type MainPageTypes = {
    placesCount: number;
@@ -11,11 +12,7 @@ function MainPage({ placesCount }: MainPageTypes): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
+            <Logo />
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">

ac41e5c20699468e8433880e04cbdd9471693b5d
dimpulsha <dimpulsha@gmail.com>
2022-07-12 18:16:30 +0300

    добавляет базовый роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../config';
 import MainPage from '../../pages/main-page/main-page';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Property from '../../pages/property/property';
+import ErrorPage from '../../pages/error-page/error-page';
 
 type AppPropsTypes = {
    placesCount: number;
@@ -7,7 +13,16 @@ type AppPropsTypes = {
 function App({ placesCount }: AppPropsTypes): JSX.Element {
 
   return (
-    <MainPage placesCount = { placesCount } />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainPage placesCount={placesCount} />} />
+        <Route path={AppRoute.Login} element={<Login />} />
+        <Route path={AppRoute.Error} element={<ErrorPage />} />
+        <Route path={AppRoute.Room} element={<Property />} />
+        <Route path={AppRoute.Favorites} element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ project/src/config.ts
@@ -0,0 +1,7 @@
+export enum AppRoute {
+   Login = 'login',
+   Error = '404',
+   Favorites = 'result',
+   Main = '/',
+   Room = 'offer/:id ',
+ }
